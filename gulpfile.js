1831d87552f1648ace60c9ba8390fd4928cc8e7d
adrigm <adrigm.admin@gmail.com>
2015-12-11 15:27:35 +0100

    Añadidas tares js y clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
-var gulp = require('gulp');
+var gulp =      require('gulp');
+var concat =    require('gulp-concat');
+var uglify =    require('gulp-uglify');
+var del =       require('del');
 
 gulp.task('html', function() {
     gulp.src('src/html/**/*.html')
         .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('js', function() {
+    gulp.src('src/js/**/*.js')
+        .pipe(concat('final.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('clean', function() {
+    return del([
+        'dist'
+    ]);
+});
+
+gulp.task('default', ['html', 'js']);
