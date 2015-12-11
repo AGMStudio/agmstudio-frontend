0596d562a7a06a6d98033b6a7c92460d568b409f
adrigm <adrigm.admin@gmail.com>
2015-12-11 18:53:14 +0100

    Añadido path

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,41 @@ var gulp =          require('gulp');
 var concat =        require('gulp-concat');
 var uglify =        require('gulp-uglify');
 var del =           require('del');
+var path =          require('path');
 var runSequence =   require('run-sequence');
+
+var config = {
+    folders :  {
+        dist : 'dist',
+        assets: 'assets'
+    }
+};
+
+var paths = {
+    dist : path.join(config.folders.dist),
+    assets : path.join(config.folders.dist, config.folders.assets),
+    html : path.join(config.folders.dist),
+    js : path.join(config.folders.dist, config.folders.assets, 'js'),
+    fonts : path.join(config.folders.dist, config.folders.assets, 'fonts'),
+    css : path.join(config.folders.dist, config.folders.assets, 'css'),
+    img : path.join(config.folders.dist, config.folders.assets, 'img'),
+};
 
 gulp.task('html', function() {
     gulp.src('src/html/**/*.html')
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('js', function() {
     gulp.src('src/js/**/*.js')
         .pipe(concat('final.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js/'));
+        .pipe(gulp.dest(paths.js));
 });
 
 gulp.task('clean', function() {
     return del([
-        'dist'
+        paths.dist
     ]);
 });
 
