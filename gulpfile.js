d3d4a500a521b993fb44bdcf1a1008a06b04c05d
adrigm <adrigm.admin@gmail.com>
2015-12-14 00:15:12 +0100

    Añadido HTML básico y tarea plugins

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,27 @@ var config = {
     folders :  {
         dist : 'dist',
         assets: 'assets'
+    },
+
+    plugins : {
+        js : [
+            'bower_components/html5shiv/dist/html5shiv.min.js',
+            'bower_components/respond/dest/respond.min.js'
+        ],
+        jsConcat : [
+            'bower_components/jquery/dist/jquery.min.js',
+            'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        ],
+        css : [
+            'bower_components/bootstrap/dist/js/bootstrap.min.css',
+            'bower_components/font-awesome/css/font-awesome.min.css',
+        ],
+        fonts : [
+            'bower_components/bootstrap/dist/fonts/*',
+            'bower_components/font-awesome/fonts/*'
+        ],
+        img : [
+        ]
     }
 };
 
@@ -23,18 +44,40 @@ var paths = {
     img : path.join(config.folders.dist, config.folders.assets, 'img'),
 };
 
-var environments = {
+var targets = {
     dist : {
+        environment: 'dist',
         data: {
             assets: config.folders.assets,
         },
     },
     dev : {
+        environment: 'dev',
         data: {
             assets: config.folders.assets,
         },
     },
 };
+
+gulp.task('plugins', function() {
+    gulp.src(config.plugins.jsConcat)
+        .pipe(concat('plugins.js', {}))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.js));
+
+    gulp.src(config.plugins.js)
+        .pipe(gulp.dest(paths.js));
+
+    gulp.src(config.plugins.css)
+        .pipe(concat('plugins.css', {}))
+        .pipe(gulp.dest(paths.css));
+
+    gulp.src(config.plugins.fonts)
+        .pipe(gulp.dest(paths.fonts));
+
+    gulp.src(config.plugins.img)
+        .pipe(gulp.dest(paths.img));
+});
 
 gulp.task('html', function() {
     gulp.src(['src/html/**/*.html', '!src/html/layout/**/*'])
@@ -42,8 +85,8 @@ gulp.task('html', function() {
             recursive: true,
             process: true,
             strip: true,
-            environment: environments.dist,
-            data: environments.dist.data,
+            environment: targets.dev.environment,
+            data: targets.dev.data
         }))
         .pipe(gulp.dest(path.join(paths.html)));
 });
@@ -64,6 +107,6 @@ gulp.task('clean', function() {
 gulp.task('default', function() {
     runSequence(
         'clean',
-        ['html', 'js']
+        ['plugins', 'html', 'js']
     );
 });
