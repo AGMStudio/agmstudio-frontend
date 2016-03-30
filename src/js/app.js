72e0401e770370445487a48cfdb2ba397dbba78c
adrigm <adrigm.admin@gmail.com>
2016-03-31 00:43:56 +0200

    updatte portfolio

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,23 @@
-/* Change navbar with scroll */
-var navbar_selector = $('.agm-navbar');
+$(document).ready(function(){
+    /* Change navbar with scroll */
+    var navbar_selector = $('.agm-navbar');
 
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 120) {
-        navbar_selector.addClass('shrink animated fadeInDown');
-        navbar_selector.addClass('navbar-fixed-top');
-        navbar_selector.removeClass('navbar-static-top');
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 120) {
+            navbar_selector.addClass('shrink animated fadeInDown');
+            navbar_selector.addClass('navbar-fixed-top');
+            navbar_selector.removeClass('navbar-static-top');
 
-    } else {
-        navbar_selector.removeClass('shrink animated fadeInDown');
-        navbar_selector.addClass('navbar-static-top');
-        navbar_selector.removeClass('navbar-fixed-top');
-    }
+        } else {
+            navbar_selector.removeClass('shrink animated fadeInDown');
+            navbar_selector.addClass('navbar-static-top');
+            navbar_selector.removeClass('navbar-fixed-top');
+        }
+    });
+
+    var potfolio_more_link = $('.portfolio-item-more');
+    potfolio_more_link.on('click', function(e){
+        $(this).parent().parent().toggleClass('open');
+        e.preventDefault();
+    });
 });
