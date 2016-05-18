38071ddc760f66d557400cde913514fc808dfd4c
adrigm <adrigm.admin@gmail.com>
2016-05-18 07:19:42 +0200

    Fixed navbar mode margin

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
     /* Change navbar with scroll */
     var navbar_selector = $('.agm-navbar');
+    var body_selector = $('body');
 
     $(window).scroll(function() {
         if ($(document).scrollTop() > 120) {
             navbar_selector.addClass('shrink animated fadeInDown');
             navbar_selector.addClass('navbar-fixed-top');
             navbar_selector.removeClass('navbar-static-top');
+            body_selector.addClass('navbar-fixed-mode');
 
         } else {
             navbar_selector.removeClass('shrink animated fadeInDown');
             navbar_selector.addClass('navbar-static-top');
             navbar_selector.removeClass('navbar-fixed-top');
+            body_selector.removeClass('navbar-fixed-mode');
         }
     });
 
